75a4b4a45d0fa03c6cde45e45b9ed37e285f71c8
claireyyu <claireyueyuca@gmail.com>
2024-10-03 12:14:15 -0700

    Use conditional rendering and navigation.push

--- Components/Home.js
+++ Components/Home.js
@@ -61,7 +61,7 @@ export default function Home({ navigation }) {
   }
 
   const pressHandler = (pressedItem) => {
-    navigation.navigate('Details', {pressedItem})
+    navigation.navigate('Details', {currentItem: pressedItem})
   }
 
   return (
