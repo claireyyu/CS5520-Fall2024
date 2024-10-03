fa8ea578e406b21a08c98e3ef40fa80dd8ceabd1
claireyyu <claireyueyuca@gmail.com>
2024-10-03 15:44:37 -0700

    Add screenOptions prop for navigator

--- App.js
+++ App.js
@@ -8,17 +8,16 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{
+        headerStyle: { backgroundColor: 'lightseagreen' },
+        headerTintColor: 'white'
+      }}>
         <Stack.Screen name="Home" component={Home}
           options={{
-            title: "All My Goals",
-            headerStyle: { backgroundColor: 'lightseagreen' },
-            headerTintColor: 'white'
+            title: "All My Goals"
           }}/>
         <Stack.Screen name="Details" component={GoalDetails} options={({route, navigation}) => ({
           title: route.params ? `${route.params.currentItem.text}` : 'More Details',
-          headerStyle: { backgroundColor: 'lightseagreen' },
-          headerTintColor: 'white',
           headerRight: () => (
             <Button title="Warning" onPress={() => (console.log('Warning'))} color="white"/>
           )
