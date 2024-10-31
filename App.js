9ab595ca710beed97a5b151879341158ec8407b4
claireyyu <claireyueyuca@gmail.com>
2024-10-31 12:00:09 -0700

    Add firebase for authentication

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Components/Home'
 import GoalDetails from './Components/GoalDetails'
-import { Button } from 'react-native'
+import { Button } from 'react-native';
+import SignUpForm from './Components/SignUpForm';
+import LoginForm from './Components/LoginForm';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -13,6 +15,15 @@ export default function App() {
         headerStyle: { backgroundColor: 'purple' },
         headerTintColor: 'white'
       }}>
+        <Stack.Screen name="Login" component={LoginForm}
+          options={({route, navigation}) => ({
+            title: 'Login',
+          })}
+        />
+        <Stack.Screen name="SignUp" component={SignUpForm}
+        options={({route, navigation}) => ({
+          title: 'Sign Up',
+        })}/>
         <Stack.Screen name="Home" component={Home}
           options={{
             title: "All My Goals"
