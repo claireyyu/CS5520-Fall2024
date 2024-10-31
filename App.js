747ed80a7b6cc92e6b21c4b0d25c1b8943db93d4
claireyyu <claireyueyuca@gmail.com>
2024-10-31 12:07:33 -0700

    Implement authentication flow with conditional rendering for login and signup screens

--- App.js
+++ App.js
@@ -3,11 +3,45 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Components/Home'
 import GoalDetails from './Components/GoalDetails'
 import { Button } from 'react-native';
+import React, { useState } from 'react';
 import SignUpForm from './Components/SignUpForm';
 import LoginForm from './Components/LoginForm';
 
+const Stack = createNativeStackNavigator();
+
+
+const AuthStack = (
+  <>
+    <Stack.Screen name="Login" component={LoginForm}
+      options={({route, navigation}) => ({
+        title: 'Login',
+      })}
+    />
+    <Stack.Screen name="SignUp" component={SignUpForm}
+    options={({route, navigation}) => ({
+      title: 'Sign Up',
+    })} />
+  </>
+
+)
+
+const AppStack = (
+  <>
+    <Stack.Screen name="Home" component={Home}
+      options={{
+        title: "All My Goals"
+      }}/>
+    <Stack.Screen name="Details" component={GoalDetails} options={({route, navigation}) => ({
+      title: route.params ? `${route.params.currentItem.text}` : 'More Details',
+    })} />
+  </>
+)
+
 export default function App() {
-  const Stack = createNativeStackNavigator();
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+
+
 
   return (
     <NavigationContainer>
@@ -15,7 +49,7 @@ export default function App() {
         headerStyle: { backgroundColor: 'purple' },
         headerTintColor: 'white'
       }}>
-        <Stack.Screen name="Login" component={LoginForm}
+        {/* <Stack.Screen name="Login" component={LoginForm}
           options={({route, navigation}) => ({
             title: 'Login',
           })}
@@ -30,7 +64,8 @@ export default function App() {
           }}/>
         <Stack.Screen name="Details" component={GoalDetails} options={({route, navigation}) => ({
           title: route.params ? `${route.params.currentItem.text}` : 'More Details',
-        })} />
+        })} /> */}
+        {isUserLoggedIn ? AppStack : AuthStack}
       </Stack.Navigator>
     </NavigationContainer>
   )
